a21058049182e2d671a04865758d5ea79c585718
drlsdee <tracert0@gmail.com>
2020-08-04 15:59:24 +0500

    "Get-RebootDelay", "Show-MessageToAll"
    "Get-RebootDelay": returns TRUE if the delay between the current date and the date of the last registered event is more than 3 business days.
    "Show-MessageToAll": just shows the message. TODO: move the messages content to culture-dependent files.

---
+++ WuReport-Client/Functions/Public/Get-RebootDelay.ps1
@@ -0,0 +1,61 @@
+function Get-RebootDelay {
+    [CmdletBinding()]
+    param (
+        # Start datestamp; must be RFC string
+        [Parameter()]
+        [string]
+        $StartDateString,
+
+        # Weekends
+        [Parameter()]
+        [ValidateSet(
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+        )]
+        [string[]]
+        $Weekends   = @('Sunday', 'Saturday')
+    )
+    [string]$myName         = "$($MyInvocation.InvocationName):"
+    Write-Verbose -Message  "$myName Starting the function..."
+
+    try {
+        [datetime]$dateStart    = [datetime]::Parse($StartDateString)
+    }
+    catch {
+        Write-Warning -Message "$myName Unable to parse timestamp: $($StartDateString); Exception: $($_.Exception.Message)"
+        return $false   #   Here we can't assume the reboot is really needed.
+    }
+
+    [datetime]$dateCurrent      = [datetime]::Now
+
+    if ($dateStart -gt $dateCurrent) {
+        Write-Warning -Message "$myName The last date of registered event (pending reboot, pending updates) is in future: $dateStart"
+        return $false   #   Here we can't assume the reboot is really needed.
+    }
+
+    #   I can't find right now a suitable method to evaluate if the one day from the time span is a weekend or business day. So deal with a loop "do-while".
+    [datetime]$dayToEval    = $dateStart
+    [int]$businessDays      = 0
+    do {
+        [string]$dayOfWeek  = $dayToEval.DayOfWeek
+        if (-not ($Weekends.Contains($dayOfWeek))) {
+            $businessDays   = $businessDays + 1
+        }
+        $dayToEval          = $dayToEval.AddDays(1)
+    } while ($dayToEval -le $dateCurrent)
+    Write-Verbose -Message "$myName Days total: $(($dateCurrent - $dateStart).Days); business days: $($businessDays)"
+
+    if ($businessDays -lt 3) {
+        Write-Verbose -Message "$myName Delay is less than the limit value."
+        return $false
+    }
+    else {
+        Write-Verbose -Message "$myName Delay reached the limit value. Pending actions will be executed immediately."
+        return $true
+    }
+}
---
+++ WuReport-Client/Functions/Public/Show-MessageToAll.ps1
@@ -0,0 +1,70 @@
+function Show-MessageToAll {
+    [CmdletBinding()]
+    param (
+        # User list
+        [Parameter()]
+        [string[]]
+        $Users,
+
+        # Reason for message: Pending Reboot, Pending Updates, Immediate Reboot, Immediately Installing Updates
+        [Parameter()]
+        [ValidateSet(
+            'RebootPending',
+            'RebootNow',
+            'UpdatesPending',
+            'UpdateNow'
+        )]
+        [string]
+        $Reason,
+
+        # Time to display
+        [Parameter()]
+        [int]
+        $TimeToDisplay  = 180
+    )
+    [string]$myName         = "$($MyInvocation.InvocationName):"
+    Write-Verbose -Message  "$myName Starting the function..."
+
+    switch ($Reason) {
+        'RebootPending'     {
+            [string]$messageBody    = "$env:COMPUTERNAME is pending for reboot."
+        }
+        'RebootNow'         {
+            [string]$messageBody    = "$env:COMPUTERNAME is waiting for reboot more than 3 days and will be restarted immediately!"
+        }
+        'UpdatesPending'    {
+            [string]$messageBody    = "$env:COMPUTERNAME is waiting for installation of critical and security updates."
+        }
+        'UpdateNow'         {
+            [string]$messageBody    = "$env:COMPUTERNAME is waiting for installation of critical and security updates for more than 3 days! The updates will be installed immediately. This computer may reboot during installation."
+        }
+    }
+
+    #   Since we don't need user interaction in this case, so we'll just use good old "msg.exe".
+    #   We expect the application to be present.
+    #   If, for some reason, Microsoft decides to exclude this binary in future releases of Windows, the function in its current state will no longer work.
+    if ($Users.Count -eq 1) {
+        Write-Verbose -Message "$myName Sending message to single user: $($Users[0]); message reason: $Reason"
+        [string]$msgCommandString   = "msg.exe $($Users[0]) /TIME:$($TimeToDisplay) `"$($messageBody)`""
+        Invoke-Expression -Command $msgCommandString
+        Write-Verbose -Message "$myName The message was sent to user: $($Users[0]); message will display $TimeToDisplay seconds."
+        return  #   That's all in that case, so we can return
+    }
+
+    #   Because the "msg.exe" can send a message to a list of users only if that list is stored in file, we should create the file
+    #   (and then, after all, remove it).
+    #   To prevent the message from being sent to users whose names have been stored in the file since the last time the script was run,
+    #   we can use the GUID to name the file. Then read the file, then remove it. But the list may be really big, or, i.e.
+    #   something strange and terrible can happen during script execution, what may affect the script duration.
+    #   And the file will be removed before all users receive their messages. Oops!
+    #   Or we can just call the [System.IO.Path]::GetTempFileName() method and save the resulting string. The file will be deleted eventually by OS,
+    #   but not during the script execution. As I hope.
+    [string]$pathToUserList = [System.IO.Path]::GetTempFileName()
+    #   The method not only returns a path to a temporary file, but also creates the file.
+    Out-File -InputObject $Users -FilePath $pathToUserList
+    Write-Verbose -Message "$myName The list of $($Users.Count) usernames was stored in file: $pathToUserList"
+    [string]$msgCommandString   = "msg.exe @$($pathToUserList) /TIME:$($TimeToDisplay) `"$($messageBody)`""
+    Invoke-Expression -Command $msgCommandString
+    Write-Verbose -Message "$myName The message was sent to $($Users.Count) users; message will display $TimeToDisplay seconds."
+    return
+}
